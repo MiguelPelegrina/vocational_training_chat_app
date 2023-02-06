8342a075f8bc3d3dfc69f7467d0dd9ff1dff4439
migup <migupele@gmail.com>
2023-02-06 13:50:12 +0100

    Funciona

--- app/src/main/java/com/example/chat/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/example/chat/adapter/RecyclerAdapter.java
@@ -16,16 +16,33 @@ import com.example.chat.model.Paquete;
 
 import java.util.List;
 
+/**
+ * Clase adaptadoras encargada de mostrar tanto los mensajes recibidos como los mandados por el
+ * usuario de la aplicación
+ */
 public class RecyclerAdapter extends RecyclerView.Adapter {
+    // Atributos de la clase
+    // Estos atributos nos sirven para identificar el tipo de paquete, es decir, si es uno mandado
+    // o recibido, para posteriormente utilizar un XML u otro
     private static final int PAQUETE_MANDADO = 0;
     private static final int PAQUETE_RECIBIDO = 1;
-
+    // Atributos de la instancia
     private List<Paquete> listaPaquetes;
 
+    /**
+     * Constructor por parámetros
+     * @param listaPaquetes Lista de paquetes que se van a mostrar
+     */
     public RecyclerAdapter(List<Paquete> listaPaquetes){
         this.listaPaquetes = listaPaquetes;
     }
 
+    /**
+     * Método encargado de comprobar si el paquete que se guardará dentro de la lista pertenece
+     * al propio usuario o a otro ajeno
+     * @param position Posición del elemento dentro de la lista
+     * @return Devuelve 0 si el paquete es recibidido y 1 si el paquete es mandado
+     */
     @Override
     public int getItemViewType(int position){
         Paquete paquete = (Paquete) listaPaquetes.get(position);
@@ -38,13 +55,21 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
         return tipoMensaje;
     }
 
+    /**
+     * Método onCreate que se ejecuta al crear el ViewHolder. Infla el diseño de cada celda, la
+     * rellena con el RecyclerHolder y les asigna los oyentes necesarios. En función del tipo de
+     * paquete utiliza un layout diferente.
+     * @param parent Vista padre en la cual se mostrará el RecyclerHolder
+     * @param viewType Tipo de vista. En esta caso llama al método sobreescrito anteriormente:
+     *                 getItemViewType()
+     * @return Devuelve el objeto de la clase viewHolder creado
+     */
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view;
         RecyclerView.ViewHolder viewHolder = null;
 
-        // TODO
         switch (viewType) {
             case PAQUETE_MANDADO:
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_self, parent, false);
@@ -59,6 +84,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
         return viewHolder;
     }
 
+    /**
+     * Método onBind que se encarga rellenar los elementos de la celda creada previamente con la
+     * información de la lista de elementos asignada en el constructor.
+     * @param holder RecyclerHolder que contiene los componentes que mostrarán la información
+     * @param position Position del elemento en la lista de elementos
+     */
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         Paquete paquete = listaPaquetes.get(position);
@@ -72,15 +103,28 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
         }
     }
 
+    /**
+     * Método que devuelve el tamaño de la lista de paquetes
+     * @return Devuelve el número de elementos de la lista
+     */
     @Override
     public int getItemCount() {
         return listaPaquetes.size();
     }
 
+    // Clases internas
+    // TODO SERÍA MEJORAR CREARNOS UNA CLASE PADRE???
+    /**
+     * Clase de tipo RecyclerHolder que extiende de ViewHolder. Corresponde a los mensajes mandados
+     */
     private class RecyclerHolderSender extends RecyclerView.ViewHolder{
         // Atributos de la clase
         TextView tvMessage;
 
+        /**
+         * Constructor por parámetro
+         * @param itemView Vista del layout
+         */
         public RecyclerHolderSender(@NonNull View itemView) {
             super(itemView);
 
@@ -88,10 +132,17 @@ public class RecyclerAdapter extends RecyclerView.Adapter {
         }
     }
 
+    /**
+     * Clase de tipo RecyclerHolder que extiende de ViewHolder. Corresponde a los mensajes recibidos
+     */
     private class RecyclerHolderReceiver extends RecyclerView.ViewHolder{
         // Atributos de la clase
         TextView tvMessage;
 
+        /**
+         * Constructor por parámetro
+         * @param itemView Vista del layout
+         */
         public RecyclerHolderReceiver(@NonNull View itemView) {
             super(itemView);
 
