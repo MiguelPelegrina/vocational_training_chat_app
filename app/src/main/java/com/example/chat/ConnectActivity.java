707eba725bf47ee03bb238c32260831da1d68839
migup <migupele@gmail.com>
2023-02-08 10:21:40 +0100

    Commentando

--- app/src/main/java/com/example/chat/ConnectActivity.java
+++ app/src/main/java/com/example/chat/ConnectActivity.java
@@ -27,12 +27,8 @@ public class ConnectActivity extends AppCompatActivity {
     // Variables de clase
     public static final int SERVER_PORT = 1234;
     public static String USER_IP = "";
-    public static String OTHER_IP = "";
     // Variables de la instancia
-    private ImageButton btnSend;
-    //TODO
     private Button btnConnect;
-    private TextView txtConection;
     private TextView txtName;
     private TextView txtIpSelf;
     private TextView txtIpOther;
@@ -40,37 +36,16 @@ public class ConnectActivity extends AppCompatActivity {
 
     private Conection conection;
 
-    private ArrayList<Paquete> listaPaquetes = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private RecyclerAdapter recyclerAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
 
         // Inicialización de las variables
-        btnSend = findViewById(R.id.btnSend);
-        //TODO
         btnConnect = findViewById(R.id.btnConnect);
-        txtConection = findViewById(R.id.txtConection);
         txtName = findViewById(R.id.txtNombre);
         txtIpSelf = findViewById(R.id.txtIpSelf);
         txtIpOther = findViewById(R.id.txtIpOther);
-        txtMensaje = findViewById(R.id.txtMensaje);
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerAdapter = new RecyclerAdapter(listaPaquetes);
-
-        // Configuramos el layoutManager
-        // Nos creamos un LayoutManager, en este caso linear
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        // Invertimos el orden en el que se muestran los elementos de la lista. Los primeros
-        // elementos se muestran al final del recyclerView
-        layoutManager.setStackFromEnd(true);
-        layoutManager.setReverseLayout(true);
-        // Configuramos el recyclerView asignandole el adapter y el layoutManager
-        recyclerView.setAdapter(recyclerAdapter);
-        recyclerView.setLayoutManager(layoutManager);
 
         // Obtenemos la IP del usuario
         USER_IP = getUserIpAddress();
@@ -86,57 +61,8 @@ public class ConnectActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Paquete paquete = conection.getPaquete();
-
                 // TODO
-                Toast.makeText(ConnectActivity.this,"ha connectado con " + paquete.getIpOther(), Toast.LENGTH_LONG).show();
-
-                listaPaquetes.add(0, paquete);
-                recyclerAdapter.notifyDataSetChanged();
-                //recyclerView.swapAdapter(recyclerAdapter, true);
-                //recyclerView.scrollBy(0,0);
-                scrollToBottom();
-            }
-        });
-
-        //
-        txtIpOther.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                update();
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                update();
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                update();
-            }
-        });
-
-        //
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!txtMensaje.getText().toString().isEmpty() && !txtName.getText().toString().isEmpty()){
-                    // TODO Se podría informar al usuario con mayor precisión cuando solo el nombre
-                    // o solo el mensaje están vacios
-                    Paquete datos = new Paquete(txtName.getText().toString(), USER_IP, txtIpOther.getText().toString(), txtMensaje.getText().toString());
-                    conection.sendMessage(datos, txtIpOther.getText().toString(), new Runnable(){
-                        @Override
-                        public void run() {
-                            txtConection.setText("Conectado");
-                            listaPaquetes.add(0, datos);
-                        }
-                    });
-                    recyclerAdapter.notifyDataSetChanged();
-                    //recyclerView.swapAdapter(recyclerAdapter,true);
-                    //recyclerView.scrollBy(0,0);
-                    scrollToBottom();
-                }else{
-                    Toast.makeText(ConnectActivity.this,"Su nombre y el campo no pueden " +
-                            "estar vacios", Toast.LENGTH_LONG).show();
-                }
+                Toast.makeText(ConnectActivity.this,"Ha connectado con " + paquete.getIpOther(), Toast.LENGTH_LONG).show();
             }
         });
 
@@ -144,7 +70,7 @@ public class ConnectActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // TODO
-                Paquete datos = new Paquete(txtName.getText().toString(), USER_IP, txtIpOther.getText().toString(), txtMensaje.getText().toString());
+                Paquete datos = new Paquete(txtName.getText().toString(), USER_IP, txtIpOther.getText().toString(), "");
                 conection.sendMessage(datos, txtIpOther.getText().toString(), new Runnable(){
                     @Override
                     public void run() {
@@ -167,38 +93,13 @@ public class ConnectActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Paquete paquete = conection.getPaquete();
-
                 // TODO
-                Toast.makeText(ConnectActivity.this,"ha connectado con " + paquete.getIpOther(), Toast.LENGTH_LONG).show();
-
-                listaPaquetes.add(0, paquete);
-                recyclerAdapter.notifyDataSetChanged();
-                //recyclerView.swapAdapter(recyclerAdapter, true);
-                //recyclerView.scrollBy(0,0);
-                scrollToBottom();
+                Toast.makeText(ConnectActivity.this,"Ha connectado con " + paquete.getIpOther(), Toast.LENGTH_LONG).show();
             }
         });
     }
 
     //Métodos auxiliares
-    /**
-     * Método encargado de modificar el estado de la conexión cada vez que se modifica la IP a la
-     * que se desea mandar un mensaje.
-     */
-    private void update() {
-        txtConection.setText("Sin conectar");
-    }
-
-    //TODO
-    /**
-     * Método encargado de desplazarse a la "primera" posición del RecyclerView. Si se invierte el
-     * orden con el método setReverseLayout() del LayoutManager el usuario no tiene que "bajar" de
-     * forma manual para ver los mensajes más recientes.
-     */
-    private void scrollToBottom(){
-        recyclerView.scrollToPosition(0);
-    }
-
     /**
      * Método encargado de obtener la IP actual del usuario
      * @return Devuelve la IP del usuario
